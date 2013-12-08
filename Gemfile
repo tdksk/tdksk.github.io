2cc8e8a7e1cc593fc1c9c0ac84ecf3f751ca2dfa
Keisuke Tada <tdkskdt@gmail.com>
2013-12-08 20:56:39 +0900

    Clean up

--- Gemfile
+++ Gemfile
@@ -1,14 +1,7 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
 gem "middleman", "~>3.2.0"
 gem "middleman-blog", "~>3.5.0"
+gem "middleman-livereload", "~> 3.1.0"
 gem "slim", "~>2.0.2"
 gem "redcarpet", "~>3.0.0"
-
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
-
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -1,80 +1,31 @@
-###
-# Compass
-###
+Time.zone = "Tokyo"
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+page "/posts/*.html", layout: :post
+page "/feed.xml", layout: false
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
-Slim::Engine.set_default_options :pretty => true
-Slim::Engine.set_default_options :shortcut => {
-  '#' => {:tag => 'div', :attr => 'id'},
-  '.' => {:tag => 'div', :attr => 'class'},
-  '&' => {:tag => 'input', :attr => 'type'}
+Slim::Engine.set_default_options pretty: true
+Slim::Engine.set_default_options shortcut: {
+  '#' => {tag: 'div', attr: 'id'},
+  '.' => {tag: 'div', attr: 'class'},
+  '&' => {tag: 'input', attr: 'type'}
 }
 
-set :markdown, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true, :with_toc_data => true, :smartypants => true
+set :markdown, tables: true, autolink: true, gh_blockcode: true, fenced_code_blocks: true, with_toc_data: true, smartypants: true
 set :markdown_engine, :redcarpet
 
-# Build-specific configuration
+activate :livereload
+
+activate :blog do |blog|
+  blog.sources = "posts/:year-:month-:day-:title.html"
+  blog.default_extension = ".md"
+  blog.paginate = false
+end
+
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :minify_css
+  activate :minify_javascript
 end
